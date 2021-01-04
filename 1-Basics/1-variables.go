cf78364be159e415962007f11780ad4b351726d0
hauntarl <sameer.mungole@gmail.com>
2021-01-04 17:12:15 +0530

    formatted the code

--- 1-Basics/1-variables.go
+++ 1-Basics/1-variables.go
@@ -5,6 +5,25 @@ import (
 	"strconv"
 )
 
+/*
+	VARIABLE DECLARATION:
+		1. var i int (defaults to 0)
+		2. var i int = 0
+		3. var i = 0
+		4. i := 0 (type inference)(not allowed when declaring global variables)
+
+	VISIBILTY:
+		1. no private scope
+		2. local variables are block scoped
+		3. lower case global variables have package scope
+		4. upper case variables are exported
+
+	NAMING CONVENTION:
+		1. Pascal for Exporting variables
+		2. camelCase for rest
+		3. Capitalize acronyms (theHTTP, theURL)
+*/
+
 // K is exported (good practice to add comment before exported variable)
 var K int // global variable, scope: exported outside package
 
@@ -13,46 +32,30 @@ var k int // global variable, scope: current package
 var ( // declaring mutliple global variables at once
 	author string = "hauntarl"
 	age    int    = 22
-	theDOB int    = 1997 // acronyms in capital
+	theDOB        = 1997 // acronyms in capital
 )
 
 func main() {
+	// using global variables
+	fmt.Printf(
+		"author: %v, age: %v, dob: %v, exported K: %v\n",
+		author, age, theDOB, K,
+	)
+
+	i := 420 // local variable, scope: block
+	cout(i)
+
+	var j float32  // if declared, local variable must be used.
+	j = float32(i) // requires explicit conversion to make sure no data is lost
+	cout(j)
+
 	/*
-		VARIABLE DECLARATION:
-			1. var i int (defaults to 0)
-			2. var i int = 0
-			3. var i = 0
-			4. i := 0 (type inference)(not allowed when declaring global variables)
-
-		VISIBILTY:
-			1. no private scope
-			2. local variables are block scoped
-			3. lower case global variables have package scope
-			4. upper case variables are exported
-
-		NAMING CONVENTION:
-			1. Pascal for Exporting variables
-			2. camelCase for rest
-			3. Capitalize acronyms (theHTTP, theURL)
-
 		TYPE CONVERSION:
 			1. destinationType(variable)
 			2. use strconv package to convert numbers to string
 			3. alternatively, use fmt.Sprintf() to convert variables to string
 			NOTE: no implicit type conversion
 	*/
-
-	i := 420 // local variable, scope: block
-	cout(i)
-
-	var j float32 // if declared, local variable must be used.
-
-	// using global variables
-	fmt.Printf("author: %v, age: %v, dob: %v, exported K: %v\n", author, age, theDOB, K)
-
-	j = float32(i) // requires explicit conversion to make sure no data is lost
-	cout(j)
-
 	k := string(i) // will convert number to its respective unicode character
 	cout(k)
 
@@ -63,6 +66,4 @@ func main() {
 	cout(m)
 }
 
-func cout(val interface{}) {
-	fmt.Printf("%v, %T\n", val, val)
-}
+func cout(val interface{}) { fmt.Printf("%v, %T\n", val, val) }
