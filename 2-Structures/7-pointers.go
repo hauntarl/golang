7524e7a20f097ffa6ee4c3e1758be021c2cdda64
hauntarl <sameer.mungole@gmail.com>
2021-01-05 21:05:52 +0530

    added pointers section

--- 2-Structures/7-pointers.go
+++ 2-Structures/7-pointers.go
@@ -2,32 +2,33 @@ package main
 
 import "fmt"
 
+/*
+	Creating Pointers:
+		1. var <var2> *<datatype> = &<var1>
+			- * declaring a pointer to data of that type
+			- & is addressof operator
+		2. <var2> := &<var1>
+
+	Dereferencing Pointers:
+		- *<var2> we ask go to look into the value stored in that address
+		- complex types (eg. structs) are automatically dereferenced
+
+	The new function:
+		- many cases you want a variable to point at a memory location
+		- new(<datatype>) creates a new object in memory, initializes it with
+		  default value and returns the address of that memory
+
+	Types with internal pointers:
+		- all assignment operations in go are copy operations
+		- slices, maps (shallow copy, as they have internal pointers)
+		- primitives, arrays and structs (deep copy)
+*/
+
 type myStruct struct {
 	foo int
 }
 
 func main() {
-	/*
-		Creating Pointers:
-			1. var <var2> *<datatype> = &<var1>
-				- * declaring a pointer to data of that type
-				- & is addressof operator
-			2. <var2> := &<var1>
-
-		Dereferencing Pointers:
-			- *<var2> we ask go to look into the value stored in that address
-			- complex types (eg. structs) are automatically dereferenced
-
-		The new function:
-			- many cases you want a variable to point at a memory location
-			- new(<datatype>) creates a new object in memory, initializes it with
-			  default value and returns the address of that memory
-
-		Types with internal pointers:
-			- all assignment operations in go are copy operations
-			- slices, maps (shallow copy, as they have internal pointers)
-			- primitives, arrays and structs (deep copy)
-	*/
 	var a int = 42
 	var b *int = &a
 	fmt.Printf("a = %v, *b = %v\t &a = %v, b = %v\n", a, *b, &a, b) // * dereferencing operator
